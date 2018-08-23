1acfb509bf2c9e2fc0eb171e722ad40d04734a98
Hello World <LYZhelloworld@users.noreply.github.com>
2018-08-23 11:46:30 +0800

    Submission: Two Sum II - Input array is sorted

---
+++ two-sum-ii-input-array-is-sorted/solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        index1 = 0
+        index2 = len(numbers) - 1
+
+        while index1 < index2:
+            difference = target - numbers[index2]
+            if difference == numbers[index1]:
+                return [index1 + 1, index2 + 1]
+
+            if difference < numbers[index1]:
+                index2 -= 1
+            else:
+                index1 += 1
