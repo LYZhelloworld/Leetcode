a8931bf7e9490365577cf3cba1e9d6194a6d6897
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2018-09-05 10:42:55 +0800

    Create solution.sql

---
+++ consecutive-numbers/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select distinct l1.Num as ConsecutiveNums
+from `Logs` l1
+inner join `Logs` l2 on l1.Id = l2.Id + 1
+inner join `Logs` l3 on l2.Id = l3.Id + 1
+where l1.Num = l2.Num and l2.Num = l3.Num;
