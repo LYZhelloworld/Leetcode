929c7e6e73e61ca49c5e5e89415908bde424401a
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2018-09-05 14:12:32 +0800

    Create solution.sql

---
+++ human-traffic-of-stadium/solution.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select distinct s1.*
+from stadium s1, stadium s2, stadium s3
+where s1.people >= 100 and s2.people >= 100 and s3.people >= 100
+and (
+    (s1.id = s2.id - 1 and s1.id = s3.id - 2)
+    or
+    (s1.id = s2.id + 1 and s1.id = s3.id - 1)
+    or
+    (s1.id = s2.id + 2 and s1.id = s3.id + 1)
+) order by id asc;
