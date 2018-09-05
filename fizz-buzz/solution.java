3d4765f13731e5637931858beb146aaf218f87c2
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2018-09-05 16:39:24 +0800

    Create solution.java

---
+++ fizz-buzz/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new LinkedList<>();
+        for(int i = 1; i <= n; ++i) {
+            result.add(i % 15 == 0 ? "FizzBuzz" :
+                       i %  5 == 0 ? "Buzz" :
+                       i %  3 == 0 ? "Fizz" :
+                       String.valueOf(i));
+        }
+        return result;
+    }
+}
