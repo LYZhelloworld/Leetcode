531edf15c607c4a22b5f39f52994ba70153d3fb7
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2018-09-18 11:18:10 +0800

    Create solution.java

---
+++ n-ary-tree-preorder-traversal/solution.java
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        if(root == null) return new LinkedList();
+        List<Integer> res = new LinkedList();
+        res.add(root.val);
+        for(Node child : root.children) {
+            res.addAll(preorder(child));
+        }
+        return res;
+    }
+}
